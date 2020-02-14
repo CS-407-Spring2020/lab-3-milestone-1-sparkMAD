7af51527e04a2eb9d301ca345b11ba761300626c
sanghyunpark <sanwisco99@gmail.com>
2020-02-14 16:00:02 -0600

    lab 3 part 1

---
+++ app/src/main/java/com/example/lab3milestone1/ExampleDialog.java
@@ -0,0 +1,25 @@
+package com.example.lab3milestone1;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class ExampleDialog extends AppCompatDialogFragment {
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Information");
+        builder.setMessage("This is a Dialog");
+        builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        return builder.create();
+    }
+}
